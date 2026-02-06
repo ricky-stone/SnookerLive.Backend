fcf96da816dfacde0e719001dadba81409432886
ricky-stone <garyth3duck8@gmail.com>
2026-02-06 03:17:00 +0000

    Added Player Seeder

---
+++ PlayerSeeder/Service.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Rabbit;
+
+namespace SnookerLive;
+
+public sealed class Service(ILogger<Service> logger, IMessageBus bus) : BackgroundService
+{
+    private const string QueueName = "snookerorg.low";
+    private const int currentSeason = 2025;
+    private const int targetSeason = 1950;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+
+        for (var season = currentSeason; season >= targetSeason; season--)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var message = new SnookerOrgMessage("Fetch Players") { Url = ["t", "10", "s", season] };
+            logger.LogInformation("Sending sync request to {Queue} (s={season})", QueueName, season);
+            await bus.PublishAsync(QueueName, message);
+        }
+
+    }
+}
