781741e3c430886f35500473bb21a8d59193f597
ricky-stone <garyth3duck8@gmail.com>
2026-02-06 20:45:16 +0000

    Removed Candidates from project

--- CandidateSync/SnookerOrgMessage.cs
+++
@@ -1,8 +0,0 @@
-using Rabbit;
-
-namespace SnookerLive;
-
-public class SnookerOrgMessage(string action) : BaseMessage(action)
-{
-    public object?[] Url { get; set; } = [];
-}
--- CandidateWorker/SnookerOrgDataResponse.cs
+++
@@ -1,8 +0,0 @@
-using Rabbit;
-
-namespace SnookerLive;
-
-public sealed class SnookerOrgDataResponse(string action) : BaseMessage(action)
-{
-    public string Data { get; set; } = string.Empty;
-}
